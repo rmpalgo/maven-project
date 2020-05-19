6c528169fc537ff38543d381e9b5581de7d3ba68
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-19 15:05:55 -0500

    Test method for arrayEquals

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -8,30 +8,29 @@ import static org.junit.Assert.*;
 public class MyFirstTest {
 
     @Test
-    public void testIfCompanyNameIsEquals() {
-        String expected = "Codeup";
+    public void testIfStringMathces() {
         String actual = "Codeup";
 
-        assertEquals(expected, actual);
-    }
-
-// When you're using floating points values a third parameter is required (Delta) which represents a positive margin of error of decimals.
-
-    @Test
-    public void testIfChangeIsCorrect() {
-        Double price = 10.0;
-        Double discount = 4.5;
-
-        assertEquals(5.5, price - discount, 0);
-        assertEquals(5.1, price - discount, 0.5);
-        assertNotEquals(4.9, price - discount, 0.5);
+        assertEquals("Codeup", actual);
+        assertNotEquals("codeup", actual);
     }
 
     @Test
-    public void assertNotSameMethod() {
+    public void testIfListsAreNotTheSame() {
         List<String> languages = new ArrayList<>();
         List<String> moreLanguages = new ArrayList<>();
         assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void TestIfArraysAreEqual() {
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
     }
 
 
