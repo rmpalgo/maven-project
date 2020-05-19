c192c11329c26dd557c2a46bfe5b1baf9ce9eabd
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-19 11:50:01 -0500

    Utilized StrinUtils isNumeric() to determine if input is a number.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,21 @@
 import com.google.common.base.Joiner;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello Maven");
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("Type here: ");
+       String input = scanner.nextLine();
+        System.out.println(StringUtils.isNumeric(input));
 
-        Joiner joiner = Joiner.on("; ").skipNulls();
-        System.out.println(joiner.join("Harry", null, "Ron", "Hermione"));
+       if(StringUtils.isNumeric(input)) {
+           System.out.println("You Entered: " + input);
+           System.out.println("This is a number");
+       } else {
+           System.out.println(input + " " + "is not a number");
+       }
+
     }
 }
